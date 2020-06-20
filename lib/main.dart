bc0a78624f4ce6974855fed216616cfa88c34025
AMISHA NARSINGANI <amishanarsingani@gmail.com>
2020-06-21 00:49:04 +0530

    audio play functionality implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -10,7 +10,14 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: Text(nouns.first),
+            child: FlatButton(
+              onPressed: () {
+                //declaring local variable player
+                final player=AudioCache();
+                player.play('note1.wav');
+              },
+              child: Text('Click me'),
+            ),
           ),
         ),
       ),
