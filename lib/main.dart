e3353453ec11e653cb69efd2c5f42e9f3511a576
AMISHA NARSINGANI <amishanarsingani@gmail.com>
2020-06-22 21:51:18 +0530

    playing multiple sounds

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,65 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                //declaring local variable player
-                final player=AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click me'),
-            ),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  //declaring local variable player
+                  final player = AudioCache();
+                  player.play('note1.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  //declaring local variable player
+                  final player = AudioCache();
+                  player.play('note2.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  //declaring local variable player
+                  final player = AudioCache();
+                  player.play('note3.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  //declaring local variable player
+                  final player = AudioCache();
+                  player.play('note4.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.teal,
+                onPressed: () {
+                  //declaring local variable player
+                  final player = AudioCache();
+                  player.play('note5.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  //declaring local variable player
+                  final player = AudioCache();
+                  player.play('note6.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () {
+                  //declaring local variable player
+                  final player = AudioCache();
+                  player.play('note7.wav');
+                },
+              ),
+            ],
           ),
         ),
       ),
